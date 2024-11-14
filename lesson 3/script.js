aedd577e1022d50320eed0415c75b5a0952691f4
Артем Бурдикін <burdykinartem4@gmail.com>
2024-11-14 16:11:28 +0200

    Homework 3.1

---
+++ lesson 3/script.js
@@ -0,0 +1,58 @@
+// var let const - ключові слова в JS 
+// console.log('Hello');
+// let a = '1';
+// let b = '2';
+// let _ = '';
+//let $ = '';
+//let userName = '';
+
+// cebab var-ri-able - так краще писати юзернейми
+// snake var_ri_able - і так теж, там ще одна якась є
+// ?? UserName
+
+function _foo() {
+  console.log(a)
+
+}
+
+const User = function () {
+  return this
+}
+new User()
+
+
+// let var-ri-able
+
+let var_ri_able
+
+//class User {constructor() {}}
+//const User = function () {return this}
+//new User {}
+
+// let const var function class if switch case continue break default delete return else for try catch true false new
+
+// var - не варто юзати, бо оцінку понизять
+
+// let user = 'Tyoma' - user i uSer - різні змінні
+// let uSer = 'Tyoma' 
+//console.log(a);
+//var a = 'example';
+//console.log(a);
+
+// types
+
+/*
+number
+string
+boolean
+null
+undefined
+symbol
+BigInt
+*/
+
+
+/*
+Referal
+1. Object
+ */
