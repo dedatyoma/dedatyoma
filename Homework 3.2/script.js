2fc718dc47db920f59202a50e4a4b338cc79cde1
Артем Бурдикін <burdykinartem4@gmail.com>
2024-11-14 19:24:37 +0200

    Homework 3.2

--- Homework 3.2/script.js
+++ Homework 3.2/script.js
@@ -1,8 +1,19 @@
-const message1 = prompt('Write here');
-const message2 = prompt('Write here');
-const message3 = prompt('Write here');
+let line1 = prompt("Write here", "");
+let line2 = prompt("Write here", "");
+let line3 = prompt("Write here", "");
 
-let messages = [message1, message2, message3];
+let order = Math.floor(Math.random() * 6);
 
-messages.sort(() => 0.5 - Math.random());
-alert(messages);
+if (order === 0) {
+    console.log(`${line1} ${line2} ${line3}`);
+} else if (order === 1) {
+    console.log(`${line1} ${line3} ${line2}`);
+} else if (order === 2) {
+    console.log(`${line2} ${line1} ${line3}`);
+} else if (order === 3) {
+    console.log(`${line2} ${line3} ${line1}`);
+} else if (order === 4) {
+    console.log(`${line3} ${line1} ${line2}`);
+} else if (order === 5) {
+    console.log(`${line3} ${line2} ${line1}`);
+}
