f8deb0c5190f8925610810c3d55331e91612757d
Артем Бурдикін <burdykinartem4@gmail.com>
2024-11-17 11:56:21 +0200

    Homework 3.1 перездача

--- Homework 3.1/index.js
+++ Homework 3.1/index.js
@@ -1,8 +1,9 @@
-console.log(typeof 'Hello'); //string
-console.log(typeof 19); // number
-console.log(typeof 10/3 === 3); // boolean
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
-console.log(typeof function (param) {  }); // function
-console.log(typeof Symbol()); // symbol
-console.log(typeof BigInt(123)); // bigint
+const string = "Hello";
+const number = 21;
+const boolean = true;
+let myUndefined;
+const symbol = Symbol("example");
+const bigInt = 1n;
+const object = Object();
+const message4 = `JS types:\n\n${typeof string}\n${typeof number}\n${typeof boolean}\n${null}\n${typeof myUndefined}\n${typeof symbol}\n${typeof bigInt}\n${typeof object}`;
+console.log(message4);
