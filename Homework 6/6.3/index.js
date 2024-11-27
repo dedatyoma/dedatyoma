2830b4249c6892d9d1f54a076a266f8b431df047
Артем Бурдикін <burdykinartem4@gmail.com>
2024-11-27 18:09:47 +0200

    6.3

---
+++ Homework 6/6.3/index.js
@@ -0,0 +1,13 @@
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index !== -1) { 
+        array.splice(index, 1);
+    }
+    return array
+}
+
+removeElement(array, 1);
+
+alert(array); 
