fa4cf7d1f5f360161b6c342cbec70867c36e1ade
Артем Бурдикін <burdykinartem4@gmail.com>
2025-02-13 23:39:44 +0200

    class component react

--- emoji-vote/src/App.js
+++ emoji-vote/src/App.js
@@ -1,74 +1,117 @@
-import { useEffect, useState } from "react";
-import skull from "./skull.png";
-import clown from "./clown-face.png";
-import hearts from "./face-hearts_.png";
-import grinning from "./grinning-face.png";
-import joy from "./joy.png";
+import { Component } from "react";
+import skull from "./assets/skull.png";
+import clown from "./assets/clown-face.png";
+import hearts from "./assets/face-hearts_.png";
+import grinning from "./assets/grinning-face.png";
+import joy from "./assets/joy.png";
 import "./App.css";
 
-function App() {
-  const initialVotes = JSON.parse(localStorage.getItem("emojiVotes")) || {
-    skull:3,
-    clown:7,
-    hearts:6,
-    grinning:5,
-    joy:4,
-  };
-  const [votes, setVotes] = useState(initialVotes);
-  const [winner, setWinner] = useState(null);
+const emojiList = [
+  {name: 'skull', img: skull},
+  {name: 'clown', img: clown},
+  {name: 'hearts', img: hearts},
+  {name: 'grinning', img: grinning},
+  {name: 'joy', img: joy},
+];
 
-  useEffect(() => {
-    localStorage.setItem("emojiVotes", JSON.stringify(votes));
-  }, [votes]);
+class EmojiButton extends Component {
+  render() {
+    const { name, img, votes, onVote } = this.props;
+    return (
+      <li>
+        <button className="emoji-button" onClick={() => onVote(name)}>
+          <img src={img} className="App-logo" alt={name}/>
+        </button>
+        <p className="counter">{votes[name]}</p>
+      </li>
+    );
+  }
+}
 
-  const handleVote = (emoji) => {
-    setVotes((prevVotes) => ({
-      ...prevVotes,
-      [emoji]: prevVotes[emoji]+1,
+class EmojiList extends Component {
+  render() {
+    const { votes, onVote } = this.props;
+    return (
+      <div className='emoji-container'>
+        <ul className='emoji-list'>
+          {emojiList.map(({name, img}) => (
+            <EmojiButton 
+              key={name}
+              name={name}
+              img={img}
+              votes={votes}
+              onVote={onVote}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class WinnerDisplay extends Component {
+  render() {
+    const { winner } = this.props;
+    return winner ? (
+      <h3>The best Emoji is <img src={{skull, clown, hearts, grinning, joy}[winner]}/></h3>
+    ) : null;
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: JSON.parse(localStorage.getItem("emojiVotes")) || {
+        skull: 3,
+        clown: 7,
+        hearts: 6,
+        grinning: 5,
+        joy: 4,
+      },
+      winner: null
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.votes !== this.state.votes) {
+      localStorage.setItem("emojiVotes", JSON.stringify(this.state.votes));
+    }
+  }
+
+  handleVote = (emoji) => {
+    this.setState(prevState => ({
+      votes: {
+        ...prevState.votes,
+        [emoji]: prevState.votes[emoji] + 1,
+      }
     }));
   }
 
-  const showResults = () => {
-    const winnerEmoji = Object.keys(votes).reduce((max, emoji) =>
-      votes[emoji] > votes[max] ? emoji : max
+  showResults = () => {
+    const winnerEmoji = Object.keys(this.state.votes).reduce((max, emoji) =>
+      this.state.votes[emoji] > this.state.votes[max] ? emoji : max
     );
-    setWinner(winnerEmoji);
-  };  
+    this.setState({ winner: winnerEmoji });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className='emoji-container'>
-          <ul className='emoji-list'>
-            {[
-              {name: 'skull', img: skull},
-              {name: 'clown', img: clown},
-              {name: 'hearts', img: hearts},
-              {name: 'grinning', img: grinning},
-              {name: 'joy', img: joy},
-            ].map(({name, img})=> (
-              <li key={name}>
-                <button className="emoji-button" onClick={() => handleVote(name)}>
-                  <img src={img} className="App-logo" alt={name}/>
-                </button>
-                <p className="counter">{votes[name]}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <p className="sign">
-          Vote For The Best Emoji
-        </p>
-        <button className="show" alt="results" onClick={showResults}
-        >
-          Show Results
-        </button>
-        {winner && (
-          <h3>The best Emoji is <img src={{skull, clown, hearts, grinning, joy}[winner]}/></h3>
-        )}
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <EmojiList 
+            votes={this.state.votes} 
+            onVote={this.handleVote} 
+          />
+          <p className="sign">Vote For The Best Emoji</p>
+          <button className="show" alt="results" onClick={this.showResults}>
+            Show Results
+          </button>
+          <WinnerDisplay winner={this.state.winner} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
